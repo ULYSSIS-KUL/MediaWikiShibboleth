549cc3bf63df93c06aad3c32e24d6f9e9ea88d08
Joachim Vandersmissen <yoa@ulyssis.org>
2017-08-16 20:38:30 +0200

    Removed redundant function

--- includes/SpecialShibbolethLogin.php
+++ includes/SpecialShibbolethLogin.php
@@ -5,15 +5,6 @@ class SpecialShibbolethLogin extends SpecialUserLogin {
 		parent::__construct("ShibbolethLogin");
 	}
 
-	function execute($subPage) {
-		//$shib = new bKULshib();
-		//$html = '<a href="' . $shib->login_link() . '"><img src="extensions/MediaWikiShibboleth/shib.gif" alt="Central KU Leuven Login" align="middle"></a>';
-		//$output = $this->getOutput();
-		//$output->addHTML($html);
-		//print $output->getHTML();
-		parent::execute($subPage);
-	}
-
 	function getPageHtml($formHtml) {
 		$shib = new bKULshib();
 		$formHtml = '<p>Click on image to log in:</p><a href="' . $shib->login_link() . '"><img src="extensions/MediaWikiShibboleth/shib.gif" alt="Centrale KU Leuven Login" align="middle"></a><a href="#" onclick="if(document.getElementById(\'spr\').style.display==\'none\'){document.getElementById(\'spr\').style.display=\'\';document.getElementById(\'spra\').innerHTML=\'Password Login &#9660;\'}else{document.getElementById(\'spr\').style.display=\'none\';document.getElementById(\'spra\').innerHTML=\'Password Login &#9654;\'}"><p id="spra">Password Login &#9654;</p></a><div id="spr" style="display:none">' . $formHtml . '</div>';
