299d2f6e509cc9a3a308f3e676ed0374bead195a
Joachim Vandersmissen <git@joachimvandersmissen.com>
2019-02-03 19:55:09 +0100

    Moved files for version 1.2.0

---
+++ includes/MediaWikiShibboleth.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MediaWikiShibboleth;
+
+$wgRevokePermissions['Shibboleth']['editmyprivateinfo'] = true;
+$wgGroupPermissions['*']['createaccount'] = false;
+
+public static function onPersonalUrls(array &$personal_urls, Title $title, SkinTemplate $skin) {
+	unset($personal_urls['createaccount']);
+}
+
+public static function onSpecialPage_initList(&$specialPages) {
+	$specialPages['Userlogin'] = "SpecialShibbolethLogin";
+	return true;
+}
+
+public static function onUserLogout(&$user) {
+	global $wgOut;
+
+	$shib = new bKULshib();
+	if ($shib->check_login()) {
+		$wgOut->redirect($shib->logout_link());
+	}
+	return true;
+}
+
