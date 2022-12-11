4dbaa13b064bd431d0f7c5a3b85de76d733ce10b
Bert Van de Poel <bert@bhack.net>
2022-12-11 22:32:23 +0100

    Improve log in error messages when users are being denied access

--- includes/SpecialShibbolethLogin.php
+++ includes/SpecialShibbolethLogin.php
@@ -11,8 +11,7 @@ class SpecialShibbolethLogin extends SpecialUserLogin {
 	}
 
 	function errorBox($message) {
-		$errorMessage = wfMessage($message)->parse();
-		return '<table cellspacing="0" cellpadding="0" border="0" style="background: transparent; margin-top:0.5em;border:1px #b32424 solid;padding:0.5em;background-color: #fee7e6"><tr><td><b>' . $errorMessage . '</b></td></tr></table></p>';
+		return '<table cellspacing="0" cellpadding="0" border="0" style="background: transparent; margin-top:0.5em;border:1px #b32424 solid;padding:0.5em;background-color: #fee7e6"><tr><td><b>' . $message . '</b></td></tr></table></p>';
 	}
 
 	function image($shib) {
@@ -31,7 +30,7 @@ class SpecialShibbolethLogin extends SpecialUserLogin {
 		if ($shib->check_login()) {
 			global $wgMWSStudentsOnly;
 			if ($wgMWSStudentsOnly && (!$shib->is_student() || $shib->is_employee())) {
-				return $this->errorBox('mediawikishibboleth-students-only') . $this->password_login($formHtml);
+				return $this->errorBox(wfMessage('mediawikishibboleth-students-only')->parse()) . $this->password_login($formHtml);
 			}
 
 			global $wgMWSAllowedKULids;
@@ -44,7 +43,7 @@ class SpecialShibbolethLogin extends SpecialUserLogin {
 			}
 
 			if (!$found) {
-				return $this->errorBox('mediawikishibboleth-forbidden-student') . $this->password_login($formHtml);
+				return $this->errorBox(wfMessage('mediawikishibboleth-forbidden-student', $shib->kulid())->parse()) . $this->password_login($formHtml);
 			}
 
 			global $wgMWSAllowedDegrees;
@@ -58,7 +57,12 @@ class SpecialShibbolethLogin extends SpecialUserLogin {
 			}
 
 			if (!$found) {
-				return $this->errorBox('mediawikishibboleth-forbidden-degree') . $this->password_login($formHtml);
+				if (empty($oplID)) {
+					return $this->errorBox(wfMessage('mediawikishibboleth-forbidden-nodegree')->parse()) . $this->password_login($formHtml);
+				}
+				else {
+					return $this->errorBox(wfMessage('mediawikishibboleth-forbidden-degree', count($oplID), implode(', ', $oplID))->parse()) . $this->password_login($formHtml);
+				}
 			}
 
 			$this->successfulAction();
